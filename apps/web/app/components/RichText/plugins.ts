75b110e54bbe99d994f907b05cff0ccdf5cabe4a
maciejhubert <47540428+maciejhubert@users.noreply.github.com>
2025-03-27 16:40:46 +0100

    chore: add hyperlinks to text editor

--- apps/web/app/components/RichText/plugins.ts
+++ apps/web/app/components/RichText/plugins.ts
@@ -1,6 +1,7 @@
 import { TaskItem } from "@tiptap/extension-task-item";
 import { TaskList } from "@tiptap/extension-task-list";
 import { StarterKit } from "@tiptap/starter-kit";
+import { Link } from "@tiptap/extension-link";
 
 export const plugins = [
   StarterKit,
@@ -15,5 +16,13 @@ export const plugins = [
       class: "flex items-start gap-2 [&_p]:inline [&_p]:m-0",
     },
     onReadOnlyChecked: (_node, _checked) => true,
+  }),
+  Link.configure({
+    openOnClick: false,
+    HTMLAttributes: {
+      class: 'text-primary-700 underline',
+      rel: 'noopener noreferrer',
+      target: '_blank',
+    },
   }),
 ];
