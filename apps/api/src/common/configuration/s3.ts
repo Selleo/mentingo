fa8bcf2a580469d8e956224d90c5bb9b33699975
Arek K. <73478095+arekkonopka@users.noreply.github.com>
2025-05-27 14:54:49 +0200

    chore(s3): update s3 config (#443)

--- apps/api/src/common/configuration/s3.ts
+++ apps/api/src/common/configuration/s3.ts
@@ -4,10 +4,11 @@ import { type Static, Type } from "@sinclair/typebox";
 import { configValidator } from "src/utils/configValidator";
 
 const schema = Type.Object({
-  AWS_REGION: Type.String(),
-  AWS_ACCESS_KEY_ID: Type.String(),
-  AWS_SECRET_ACCESS_KEY: Type.String(),
-  AWS_S3_BUCKET_NAME: Type.String(),
+  S3_ENDPOINT: Type.String(),
+  S3_REGION: Type.String(),
+  S3_ACCESS_KEY_ID: Type.String(),
+  S3_SECRET_ACCESS_KEY: Type.String(),
+  S3_BUCKET_NAME: Type.String(),
 });
 
 type S3ConfigSchema = Static<typeof schema>;
@@ -16,10 +17,11 @@ const validateS3Config = configValidator(schema);
 
 export default registerAs("s3", (): S3ConfigSchema => {
   const values = {
-    AWS_REGION: process.env.AWS_REGION || "",
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || "",
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || "",
-    AWS_S3_BUCKET_NAME: process.env.AWS_BUCKET_NAME || "",
+    S3_ENDPOINT: process.env.S3_ENDPOINT || "",
+    S3_REGION: process.env.S3_REGION || "",
+    S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID || "",
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY || "",
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME || "",
   };
   return validateS3Config(values);
 });
