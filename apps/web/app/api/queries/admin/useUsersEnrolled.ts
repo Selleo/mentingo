6dd4b3e3bf522e92b46112a07e36d7cfa780e818
staskowsky <31994897+staskowsky@users.noreply.github.com>
2025-06-09 10:19:59 +0200

    feat(LC-635): [FE] view for enrolling students to a course by admin (#449)
    * enrolled students tab created
    
    * query & mutation created, frontend connected with backend, feature functional now

---
+++ apps/web/app/api/queries/admin/useUsersEnrolled.ts
@@ -0,0 +1,34 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { ApiClient } from "~/api/api-client";
+
+import type { GetStudentsWithEnrollmentDateResponse } from "~/api/generated-api";
+
+export type UsersEnrolledSearchParams = {
+  keyword?: string;
+  sort?: "enrolledAt" | "-enrolledAt" | undefined;
+};
+
+export const ENROLLED_USERS_QUERY_KEY = "users-enrollments";
+
+export const useUsersEnrolledQuery = (
+  courseId: string,
+  searchParams?: UsersEnrolledSearchParams,
+) => ({
+  queryKey: [ENROLLED_USERS_QUERY_KEY, searchParams],
+  queryFn: async () => {
+    const { data } = await ApiClient.api.courseControllerGetStudentsWithEnrollmentDate(courseId, {
+      ...(searchParams?.keyword && { keyword: searchParams.keyword }),
+      ...(searchParams?.sort && { sort: searchParams.sort }),
+    });
+    return data;
+  },
+  select: ({ data }: GetStudentsWithEnrollmentDateResponse) => data,
+});
+
+export function useAllUsersEnrolledSuspense(
+  courseId = "",
+  searchParams?: UsersEnrolledSearchParams,
+) {
+  return useSuspenseQuery(useUsersEnrolledQuery(courseId, searchParams));
+}
