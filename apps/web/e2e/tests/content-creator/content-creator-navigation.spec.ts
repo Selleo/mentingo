423b718f8771e94d19bae4e8beffb99f57107333
wielopolski <wielopolskim@gmail.com>
2025-07-05 23:32:14 +0200

    fix: failed navigation test

--- apps/web/e2e/tests/content-creator/content-creator-navigation.spec.ts
+++ apps/web/e2e/tests/content-creator/content-creator-navigation.spec.ts
@@ -11,7 +11,7 @@ const TEST_NAVIGATION = {
   },
   header: {
     welcomeBack: "Welcome back",
-    yourCourses: "Your courses",
+    yourCourses: "Available Courses",
     changeUserInformation: "Change user information",
   },
 } as const;
@@ -48,9 +48,9 @@ test.describe("Content creator navigation", () => {
     await page
       .getByRole("button", { name: new RegExp(TEST_NAVIGATION.button.profile, "i") })
       .click();
-    await page.waitForURL(/\/content-creators\/[a-f0-9-]{36}/);
+    await page.waitForURL(/\/profile\/[a-f0-9-]{36}/);
     const currentURL = page.url();
-    expect(currentURL).toMatch(/\/content-creators\/[a-f0-9-]{36}/);
+    expect(currentURL).toMatch(/\/profile\/[a-f0-9-]{36}/);
 
     await page
       .getByRole("button", { name: new RegExp(TEST_NAVIGATION.button.settings, "i") })
