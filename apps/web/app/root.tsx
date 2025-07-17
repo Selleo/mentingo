19985a76134626184c0ba00bf56d7d35ca59f6f8
Japrolol <148473043+Japrolol@users.noreply.github.com>
2025-07-17 14:50:29 +0200

    fix: made lesson container scrollable and made body overflow hidden

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -30,7 +30,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className={"overflow-hidden"}>
         {children}
         <ScrollRestoration />
         <Scripts />
