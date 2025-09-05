8d555f8385f11c13f148097880467d4c5fa92469
its-gabo <smolana.gabriel@gmail.com>
2025-09-05 19:38:01 +0200

    fix: translations in fill in the blanks

--- apps/web/app/modules/Courses/Lesson/Question/FillInTheBlanks/FillInTheBlanks.tsx
+++ apps/web/app/modules/Courses/Lesson/Question/FillInTheBlanks/FillInTheBlanks.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from "react-i18next";
+
 import Viewer from "~/components/RichText/Viever";
 import { Card } from "~/components/ui/card";
 
@@ -12,12 +14,16 @@ type FillInTheBlanksProps = {
 };
 
 export const FillInTheBlanks = ({ question, isCompleted }: FillInTheBlanksProps) => {
+  const { t } = useTranslation();
+
   if (!question.description) return null;
 
   return (
     <Card className="flex flex-col gap-4 border-none p-8 drop-shadow-primary">
-      <div className="details uppercase text-primary-700">Question {question.displayOrder}</div>
-      <div className="h6 text-neutral-950">Fill in the blanks.</div>
+      <div className="details uppercase text-primary-700">
+        {t("studentLessonView.other.question")} {question.displayOrder}
+      </div>
+      <div className="h6 text-neutral-950">{t("studentLessonView.other.fillInTheBlanks")}</div>
       <FillInTheTextBlanks
         content={question.description}
         replacement={(index) => {
