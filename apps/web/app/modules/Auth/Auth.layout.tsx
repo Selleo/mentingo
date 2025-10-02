71e9bc9bc5f403d052d02620004add35ac0fa700
zielvna <zielvna@gmail.com>
2025-10-02 13:09:11 +0200

    fix: add redirect from auth page if user is logged in

--- apps/web/app/modules/Auth/Auth.layout.tsx
+++ apps/web/app/modules/Auth/Auth.layout.tsx
@@ -1,6 +1,27 @@
-import { Outlet } from "@remix-run/react";
+import { Outlet, redirect } from "@remix-run/react";
+
+import { currentUserQueryOptions } from "~/api/queries";
+import { queryClient } from "~/api/queryClient";
 
 import { useCurrentUserStore } from "../common/store/useCurrentUserStore";
+
+import type { CurrentUserResponse } from "~/api/generated-api";
+
+export const clientLoader = async () => {
+  let user: CurrentUserResponse | null = null;
+
+  try {
+    user = await queryClient.ensureQueryData(currentUserQueryOptions);
+  } catch {
+    return null;
+  }
+
+  if (user) {
+    throw redirect("/");
+  }
+
+  return null;
+};
 
 export default function AuthLayout() {
   const setHasVerifiedMFA = useCurrentUserStore((state) => state.setHasVerifiedMFA);
