bd785b000a120a1f61ff6c8747ecede8f91c1a61
Jakub Zielonka <102986585+zielvna@users.noreply.github.com>
2025-10-04 20:53:17 +0200

    fix: show course overview for unregistered user if global setting is enabled (#727)

--- apps/web/app/modules/Courses/CourseView/CourseView.page.tsx
+++ apps/web/app/modules/Courses/CourseView/CourseView.page.tsx
@@ -13,6 +13,8 @@ import { MoreCoursesByAuthor } from "~/modules/Courses/CourseView/MoreCoursesByA
 import { YouMayBeInterestedIn } from "~/modules/Courses/CourseView/YouMayBeInterestedIn";
 import CertificateContent from "~/modules/Profile/Certificates/CertificateContent";
 
+import { CoursesAccessGuard } from "../Courses.layout";
+
 export default function CourseViewPage() {
   const { t } = useTranslation();
   const { id = "" } = useParams();
@@ -43,10 +45,10 @@ export default function CourseViewPage() {
   const canShowCertificate =
     course?.hasCertificate && certificate !== undefined && certificate.length > 0;
 
-  if (!course || !currentUser) return null;
+  if (!course) return null;
 
   const studentName: string =
-    certificate?.[0]?.fullName || `${currentUser.firstName} ${currentUser.lastName}`;
+    certificate?.[0]?.fullName || `${currentUser?.firstName} ${currentUser?.lastName}`;
   const courseName: string = certificate?.[0]?.courseTitle || course.title;
   const completionDate =
     certificate && certificate.length > 0
@@ -57,43 +59,45 @@ export default function CourseViewPage() {
     : "";
 
   return (
-    <PageWrapper breadcrumbs={breadcrumbs} backButton={backButton}>
-      <div className="flex w-full max-w-full flex-col gap-6 lg:grid lg:grid-cols-[1fr_480px]">
-        <div className="flex flex-col gap-y-6 overflow-hidden">
-          <CourseOverview course={course} />
-          {canShowCertificate && (
-            <div className="hidden aspect-video w-full rounded-xl border border-white bg-white shadow-sm sm:block">
-              <CertificateContent
-                studentName={studentName}
-                courseName={courseName}
-                completionDate={formattedDate}
-                backgroundImageUrl={globalSettings?.certificateBackgroundImage}
-                platformLogo={globalSettings?.platformLogoS3Key}
-              />
-            </div>
-          )}
-          <div className="flex flex-col gap-y-4 rounded-lg bg-white px-4 py-6 md:p-8">
-            <div className="flex flex-col gap-y-1">
-              <h4 className="h6 text-neutral-950">{t("studentCourseView.header")}</h4>
-              <p className="body-base-md text-neutral-800">{t("studentCourseView.subHeader")}</p>
-            </div>
-            {course.chapters?.map((chapter) => {
-              if (!chapter) return null;
-              return (
-                <CourseChapter
-                  key={chapter.id}
-                  chapter={chapter}
-                  courseId={course.id}
-                  isEnrolled={Boolean(course.enrolled)}
+    <CoursesAccessGuard>
+      <PageWrapper breadcrumbs={breadcrumbs} backButton={backButton}>
+        <div className="flex w-full max-w-full flex-col gap-6 lg:grid lg:grid-cols-[1fr_480px]">
+          <div className="flex flex-col gap-y-6 overflow-hidden">
+            <CourseOverview course={course} />
+            {canShowCertificate && (
+              <div className="hidden aspect-video w-full rounded-xl border border-white bg-white shadow-sm sm:block">
+                <CertificateContent
+                  studentName={studentName}
+                  courseName={courseName}
+                  completionDate={formattedDate}
+                  backgroundImageUrl={globalSettings?.certificateBackgroundImage}
+                  platformLogo={globalSettings?.platformLogoS3Key}
                 />
-              );
-            })}
+              </div>
+            )}
+            <div className="flex flex-col gap-y-4 rounded-lg bg-white px-4 py-6 md:p-8">
+              <div className="flex flex-col gap-y-1">
+                <h4 className="h6 text-neutral-950">{t("studentCourseView.header")}</h4>
+                <p className="body-base-md text-neutral-800">{t("studentCourseView.subHeader")}</p>
+              </div>
+              {course.chapters?.map((chapter) => {
+                if (!chapter) return null;
+                return (
+                  <CourseChapter
+                    key={chapter.id}
+                    chapter={chapter}
+                    courseId={course.id}
+                    isEnrolled={Boolean(course.enrolled)}
+                  />
+                );
+              })}
+            </div>
+            <MoreCoursesByAuthor courseId={course.id} contentCreatorId={course.authorId} />
+            <YouMayBeInterestedIn courseId={course.id} category={course.category} />
           </div>
-          <MoreCoursesByAuthor courseId={course.id} contentCreatorId={course.authorId} />
-          <YouMayBeInterestedIn courseId={course.id} category={course.category} />
+          <CourseViewSidebar course={course} />
         </div>
-        <CourseViewSidebar course={course} />
-      </div>
-    </PageWrapper>
+      </PageWrapper>
+    </CoursesAccessGuard>
   );
 }
