4f2c308ee7dd9aa800bcb7ff4af26c193b712c58
Jakub Zielonka <102986585+zielvna@users.noreply.github.com>
2025-10-15 10:05:29 +0200

    feat: display text when there are no new announcements (#779)
    * feat: display text when there are no new announcements
    
    * fix: use existing translation to empty state text in announcements page

--- apps/web/app/modules/Announcements/components/AnnouncementsList.tsx
+++ apps/web/app/modules/Announcements/components/AnnouncementsList.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from "react-i18next";
+
 import AnnouncementCard from "./AnnouncementCard";
 
 import type { GetAnnouncementsForUserResponse } from "~/api/generated-api";
@@ -8,15 +10,23 @@ interface AnnouncementsListProps {
 }
 
 export default function AnnouncementsList({ announcements, isAdminView }: AnnouncementsListProps) {
+  const { t } = useTranslation();
+
   return (
     <div className="flex w-full flex-col gap-6">
-      {announcements.map((announcement) => (
-        <AnnouncementCard
-          key={announcement.id}
-          announcement={announcement}
-          isAdminView={isAdminView}
-        />
-      ))}
+      {announcements.length ? (
+        announcements.map((announcement) => (
+          <AnnouncementCard
+            key={announcement.id}
+            announcement={announcement}
+            isAdminView={isAdminView}
+          />
+        ))
+      ) : (
+        <div className="flex justify-center mt-8">
+          {t("announcements.other.noNewAnnouncements")}
+        </div>
+      )}
     </div>
   );
 }
