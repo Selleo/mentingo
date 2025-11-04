d9cd1a4dc9c8d1d828eefd2a78fb449664fa38f9
Japrolol <148473043+Japrolol@users.noreply.github.com>
2025-11-04 17:49:26 +0100

    refactor: removed certificate options and made preview on click

--- apps/web/app/modules/Profile/Certificates/Certificate.tsx
+++ apps/web/app/modules/Profile/Certificates/Certificate.tsx
@@ -1,8 +1,5 @@
-import { Eye, Download } from "lucide-react";
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
-
 import { CertificateTrophy } from "~/assets/svgs";
+import { Button } from "~/components/ui/button";
 
 import useCertificatePDF from "./useCertificatePDF";
 
@@ -21,33 +18,6 @@ interface CertificateProps {
   }) => void;
 }
 
-interface OptionsProps {
-  handlePreviewClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
-  handleDownloadClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
-}
-
-const Options = ({ handlePreviewClick, handleDownloadClick }: OptionsProps) => {
-  const { t } = useTranslation();
-
-  return (
-    <div className="absolute right-5 top-5 z-10 w-60 rounded-md border bg-white shadow-xl">
-      <ul className="py-1">
-        <button onClick={handlePreviewClick} className="hidden w-full sm:block">
-          <li className="flex cursor-pointer items-center gap-3 px-4 py-2 hover:bg-gray-100">
-            <Eye className="size-6 text-primary-800" /> {t("studentCertificateView.button.preview")}
-          </li>
-        </button>
-        <button onClick={handleDownloadClick} className="w-full">
-          <li className="flex cursor-pointer items-center gap-3 px-4 py-2 hover:bg-gray-100">
-            <Download className="size-6 text-primary-800" />{" "}
-            {t("studentCertificateView.button.download")}
-          </li>
-        </button>
-      </ul>
-    </div>
-  );
-};
-
 const Certificate = ({
   courseName,
   courseCompletionDate,
@@ -56,21 +26,7 @@ const Certificate = ({
   backgroundImageUrl,
   onOpenCertificatePreview,
 }: CertificateProps) => {
-  const [showOptions, setShowOptions] = useState(false);
-
-  const { downloadCertificatePdf, HiddenCertificate } = useCertificatePDF();
-
-  const handleDownloadClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    if (certData) {
-      downloadCertificatePdf(courseName);
-    }
-    setShowOptions(false);
-  };
-
-  const handleOptionsCLick = () => {
-    setShowOptions(!showOptions);
-  };
+  const { HiddenCertificate } = useCertificatePDF();
 
   const handlePreviewClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -82,13 +38,7 @@ const Certificate = ({
         certData: certData,
       });
     }
-    setShowOptions(false);
   };
-
-  const displayCourseName =
-    courseName.split(" ").length > 3
-      ? courseName.split(" ").slice(0, 3).join(" ") + "..."
-      : courseName;
 
   return (
     <>
@@ -102,27 +52,17 @@ const Certificate = ({
       />
       <div className="grid grid-cols-1 gap-4">
         <div className="relative">
-          <div className="absolute -right-3 -top-3 size-9 rounded-lg border-2 border-gray-300 bg-white">
-            {showOptions && (
-              <Options
-                handlePreviewClick={handlePreviewClick}
-                handleDownloadClick={handleDownloadClick}
-              />
-            )}
-            <button
-              onClick={() => handleOptionsCLick()}
-              className="text-1xl absolute inset-0 flex rotate-90 items-center justify-center"
-            >
-              ⋯
-            </button>
-          </div>
-          <div className="border-black/60 flex min-h-24 items-center gap-2 rounded-2xl border-2 p-3">
+          <Button
+            variant="outline"
+            className="border-black/60 flex min-h-24 text-neutral text-left max-w-56 items-center gap-2 rounded-2xl border-2 p-3"
+            onClick={handlePreviewClick}
+          >
             <CertificateTrophy className="min-h-10 min-w-10" />
-            <div className="flex flex-col pl-2">
-              <h3 className="body-sm-md font-semibold">{displayCourseName}</h3>
+            <div className="flex flex-col pl-2 overflow-x-hidden">
+              <h3 className="body-sm-md font-semibold truncate">{courseName}</h3>
               <h4 className="details">{courseCompletionDate}</h4>
             </div>
-          </div>
+          </Button>
         </div>
       </div>
     </>
