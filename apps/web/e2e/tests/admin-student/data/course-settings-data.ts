79e4dd98add99d794fa7f27617d302c9ae216207
Dawid Haratyk <dawidharatyk@onet.pl>
2025-11-18 10:23:16 +0100

    refactor: tests

--- apps/web/e2e/tests/admin-student/data/course-settings-data.ts
+++ apps/web/e2e/tests/admin-student/data/course-settings-data.ts
@@ -20,6 +20,7 @@ export const COURSE_SETTINGS_UI = {
   header: {
     chapterTitle: "Artificial Intelligence in Business: Fundamentals",
     chapterTitle2: "E2E Test: Automated Course",
+    chapterTitle3: "Introduction to E2E Testing",
     settings: "settings",
     signup: "sign up",
   },
