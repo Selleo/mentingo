70296fdc2ba701138403d1beb927281c4abbaee6
Jakub Wilk <jwilk436@gmail.com>
2025-12-04 17:14:18 +0100

    fix: update aiMentor schema and references for consistency

--- apps/api/src/lesson/services/lesson.service.ts
+++ apps/api/src/lesson/services/lesson.service.ts
@@ -128,8 +128,8 @@ export class LessonService {
 
       let avatarUrl = undefined;
 
-      if (lesson.aiMentor?.avatarReference) {
-        avatarUrl = await this.fileService.getFileUrl(lesson.aiMentor.avatarReference);
+      if (lesson.aiMentor?.avatarReferenceUrl) {
+        avatarUrl = await this.fileService.getFileUrl(lesson.aiMentor.avatarReferenceUrl);
       }
 
       return {
@@ -408,8 +408,8 @@ export class LessonService {
 
     const tags = $("a").toArray();
 
-    const tagsWithImages = tags.filter((tag) =>
-      $(tag).attr("href")?.includes("/api/lesson/lesson-image"),
+    const tagsWithImages = tags.filter(
+      (tag) => $(tag).attr("href")?.includes("/api/lesson/lesson-image"),
     );
 
     tagsWithImages.forEach((tag) => {
