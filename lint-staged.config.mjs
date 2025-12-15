1dd0cd6057bd26a5dfb3693c61edebc6157ce4e6
Jakub Wilk <jwilk436@gmail.com>
2025-12-15 18:29:52 +0100

    fix: OOM error on tsc-lint-web

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,8 +1,12 @@
 export default {
-  "*.{js,jsx,ts,tsx}": [
-    () => "pnpm format:check",
-    () => "pnpm lint-tsc-web",
-    () => "pnpm lint-tsc-api",
+  "apps/web/**/*.{ts,tsx}": [
+    "pnpm exec tsc-files --noEmit --project apps/web/tsconfig.json",
+    "pnpm --filter=web exec eslint --cache",
   ],
-  "*.{json,md,css,scss,html,yml,yaml}": [() => "pnpm format:check"],
+  "apps/api/**/*.{ts,tsx}": [
+    "pnpm exec tsc-files --noEmit --project apps/api/tsconfig.json",
+    "pnpm --filter=api exec eslint --cache",
+  ],
+  "*.{js,jsx,ts,tsx}": ["pnpm exec prettier --check"],
+  "*.{json,md,css,scss,html,yml,yaml}": ["pnpm exec prettier --check"],
 };
