e3523120e29cbdab1f7a5d73bc99cae4bcdd1794
its-gabo <smolana.gabriel@gmail.com>
2025-12-19 11:17:23 +0100

    chore: remove redundant migration file after rebase

--- apps/api/src/storage/migrations/0065_add_is_mandatory_and_due_date_columns_to_group_courses.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "group_courses" ADD COLUMN "is_mandatory" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "group_courses" ADD COLUMN "due_date" timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "group_courses" DROP COLUMN IF EXISTS "settings";
