02d88159db1905e25a11bd2aed12d88a899a684f
Dawid Haratyk <dawidharatyk@onet.pl>
2026-01-02 13:37:37 +0100

    test: private course visibility

--- apps/web/e2e/db.setup.ts
+++ apps/web/e2e/db.setup.ts
@@ -6,8 +6,8 @@ import { test as setup } from "@playwright/test";
 setup("database reset", async () => {
   const clearDbAndContainers = () => {
     const apiDir = path.resolve(process.cwd(), "../api");
-    const command =
-      "docker stop $(docker ps -a -q) && docker compose down -v && docker compose up -d && pnpm db:migrate && pnpm db:seed";
+    // TODO: reset db properly
+    const command = "docker compose up -d && pnpm db:migrate && pnpm db:seed";
     execSync(command, { cwd: apiDir, stdio: "inherit", shell: true as unknown as string });
   };
 
