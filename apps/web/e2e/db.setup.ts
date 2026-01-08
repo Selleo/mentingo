b617d0910c758d4f92ed09c7e9ae9c23a1facc95
Dawid Haratyk <dawidharatyk@onet.pl>
2026-01-08 12:47:19 +0100

    refactor: removing not needed code

--- apps/web/e2e/db.setup.ts
+++ apps/web/e2e/db.setup.ts
@@ -6,7 +6,6 @@ import { test as setup } from "@playwright/test";
 setup("database reset", async () => {
   const clearDbAndContainers = () => {
     const apiDir = path.resolve(process.cwd(), "../api");
-    // TODO: reset db properly
     const command = "docker compose up -d && pnpm db:migrate && pnpm db:seed";
     execSync(command, { cwd: apiDir, stdio: "inherit", shell: true as unknown as string });
   };
