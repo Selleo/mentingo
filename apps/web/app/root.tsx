552f77550e59c99995e62e36b782d884c162398d
Gabo <smolana.gabriel@gmail.com>
2026-01-08 16:47:59 +0100

    feat: include generator metadata for better platform identification (#1174)

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -8,6 +8,8 @@ import {
   useRouteError,
 } from "@remix-run/react";
 
+import { version } from "~/../version.json";
+
 import { PlatformFavicon } from "./components/PlatformFavicon";
 import { Toaster } from "./components/ui/toaster";
 import { useNavigationTracker } from "./hooks/useNavigationTracker";
@@ -33,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="generator" content={`Mentingo LMS ${version}`} />
         <Meta />
         <Links />
       </head>
