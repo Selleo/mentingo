3aaf5fed541c200f5017b058096c648cb5071b24
mmienkoselleo <m.mienko@selleo.com>
2026-01-12 20:12:45 +0100

    fix: move course_slugs table creation to the end of migration and ensure foreign key constraint is applied correctly

--- apps/api/src/storage/migrations/0075_add_course_slugs.sql
+++ apps/api/src/storage/migrations/0075_add_course_slugs.sql
@@ -1,12 +1,3 @@
-CREATE TABLE IF NOT EXISTS "course_slugs" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"created_at" timestamp(3) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	"updated_at" timestamp(3) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	"slug" text NOT NULL,
-	"course_short_id" varchar(5) NOT NULL,
-	"lang" text NOT NULL
-);
---> statement-breakpoint
 ALTER TABLE "courses" ADD COLUMN "short_id" varchar(5);
 --> statement-breakpoint
 CREATE OR REPLACE FUNCTION generate_course_short_id() RETURNS text AS $$
@@ -54,6 +45,15 @@ UPDATE "courses" SET "short_id" = generate_course_short_id() WHERE "short_id" IS
 ALTER TABLE "courses" ALTER COLUMN "short_id" SET NOT NULL;
 --> statement-breakpoint
 ALTER TABLE "courses" ADD CONSTRAINT "courses_short_id_unique" UNIQUE("short_id");
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "course_slugs" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp(3) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"updated_at" timestamp(3) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"slug" text NOT NULL,
+	"course_short_id" varchar(5) NOT NULL,
+	"lang" text NOT NULL
+);
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "course_slugs" ADD CONSTRAINT "course_slugs_course_short_id_courses_short_id_fk" FOREIGN KEY ("course_short_id") REFERENCES "public"."courses"("short_id") ON DELETE cascade ON UPDATE cascade;
