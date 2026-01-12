d026afc31820275deeffe4af18b40973a6480f4f
mmienkoselleo <m.mienko@selleo.com>
2026-01-12 20:36:24 +0100

    fix: add foreign key constraint for course_short_id in course_slugs table creation

--- apps/api/src/storage/migrations/0075_add_course_slugs.sql
+++ apps/api/src/storage/migrations/0075_add_course_slugs.sql
@@ -52,13 +52,8 @@ CREATE TABLE IF NOT EXISTS "course_slugs" (
 	"updated_at" timestamp(3) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	"slug" text NOT NULL,
 	"course_short_id" varchar(5) NOT NULL,
-	"lang" text NOT NULL
+	"lang" text NOT NULL,
+	CONSTRAINT "course_slugs_course_short_id_courses_short_id_fk" FOREIGN KEY ("course_short_id") REFERENCES "public"."courses"("short_id") ON DELETE cascade ON UPDATE cascade
 );
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "course_slugs" ADD CONSTRAINT "course_slugs_course_short_id_courses_short_id_fk" FOREIGN KEY ("course_short_id") REFERENCES "public"."courses"("short_id") ON DELETE cascade ON UPDATE cascade;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
 --> statement-breakpoint
 CREATE UNIQUE INDEX IF NOT EXISTS "course_slug_course_short_id_lang_slug_unique_idx" ON "course_slugs" USING btree ("course_short_id","lang","slug");
