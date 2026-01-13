6008d04548eef66d3234ffef2212ab283d5e7804
Dawid Haratyk <dawidharatyk@onet.pl>
2026-01-13 17:21:58 +0100

    fix: e2e test on CI

--- apps/web/e2e/tests/admin-student/assigning-student-to-course.spec.ts
+++ apps/web/e2e/tests/admin-student/assigning-student-to-course.spec.ts
@@ -446,7 +446,7 @@ const enrollGroupToCourse = async (page: Page) => {
   await page.getByRole("button", { name: "Enroll groups" }).click();
   await page.getByRole("button", { name: "Enroll groups", exact: true }).click();
   await page.getByLabel("Enroll groups to course").getByText(GROUP.name).first().click();
-  await page.getByLabel(IDS.groupSelect).click();
+  await page.getByLabel(IDS.groupSelect).first().click();
   await page.getByRole("button", { name: "Enroll groups" }).click();
   await expect(page.getByRole("cell", { name: "Enrolled by group" })).toBeVisible();
   await logoutAdmin(page);
