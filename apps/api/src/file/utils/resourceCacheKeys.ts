96727cdfc23ba978062b718248b09e7892b007ad
its-gabo <smolana.gabriel@gmail.com>
2026-02-18 10:52:23 +0100

    refactor: update CONTEXT_TTL from seconds to milliseconds

--- apps/api/src/file/utils/resourceCacheKeys.ts
+++ apps/api/src/file/utils/resourceCacheKeys.ts
@@ -1,4 +1,4 @@
-export const CONTEXT_TTL = 86_400; // 1 day in seconds
+export const CONTEXT_TTL = 24 * 60 * 60 * 1000; // 1 day in milliseconds
 
 export function getContextKey(contextId: string) {
   return `context:${contextId}`;
