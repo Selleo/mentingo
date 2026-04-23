554ff1e5e90dedb35f2d2791f37cba41d8ced21f
Japrolol <148473043+Japrolol@users.noreply.github.com>
2026-04-23 13:36:45 +0200

    --wip-- [skip ci]

---
+++ apps/web/e2e/data/curriculum/curriculum.data.ts
@@ -0,0 +1,15 @@
+export const CURRICULUM_TEST_DATA = {
+  youtubeVideoUrl: "https://www.youtube.com/watch?v=PvB0kWs2IPQ",
+  files: {
+    image: "apps/web/e2e/data/curriculum/files/content-image.png",
+    video: "apps/web/e2e/data/curriculum/files/content-video.mp4",
+    filePreview: "apps/web/e2e/data/curriculum/files/content-file-preview.pdf",
+    fileDownload: "apps/web/e2e/data/curriculum/files/content-file-download.pdf",
+    presentationPreview: "apps/web/e2e/data/curriculum/files/content-presentation-preview.pptx",
+    presentationDownload: "apps/web/e2e/data/curriculum/files/content-presentation-download.pptx",
+    quizPhotoSingleChoice: "apps/web/e2e/data/curriculum/files/quiz-photo-single-choice.png",
+    quizPhotoMultipleChoice: "apps/web/e2e/data/curriculum/files/quiz-photo-multiple-choice.png",
+    aiMentorResource: "apps/web/e2e/data/curriculum/files/ai-mentor-resource.pdf",
+    aiMentorAvatar: "apps/web/e2e/data/curriculum/files/ai-mentor-avatar.png",
+  },
+} as const;
