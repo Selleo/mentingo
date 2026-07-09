e174acfb27a0d8321025605c31ac1d131712de49
Adam Stajer <stajeradam9@gmail.com>
2026-07-09 14:44:18 +0200

    create gamification module

---
+++ apps/api/src/gamification/gamification.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from "@nestjs/common";
+
+import { GamificationService } from "./gamification.service";
+
+@Controller("gamification")
+export class GamificationController {
+  constructor(private readonly gamificationService: GamificationService) {}
+}
