a13f58020c6577652ec8de9a90439a4124968af6
pkrzysiekk <pkrzysiek24@gmail.com>
2026-07-14 11:03:50 +0200

    fix: Add 3 separate migrations for altering announcements table

--- apps/api/src/storage/migrations/0162_replace_is_everyone_with_audience_in_announcements.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "announcements" RENAME COLUMN "is_everyone" TO "audience";--> statement-breakpoint
-ALTER TABLE "announcements" ALTER COLUMN "audience" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "announcements" ALTER COLUMN "audience" SET DEFAULT 'all_users';
