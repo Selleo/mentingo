df51abb3799f0437fd9476937f3207990913be7f
pkrzysiekk <pkrzysiek24@gmail.com>
2026-07-15 14:18:44 +0200

    feat:Add create automation repository method

--- apps/api/src/automations/repositories/automations/automations.repository.ts
+++ apps/api/src/automations/repositories/automations/automations.repository.ts
@@ -2,10 +2,24 @@ import { Inject, Injectable } from "@nestjs/common";
 
 import { DatabasePg } from "src/common";
 import { DB } from "src/storage/db/db.providers";
+import { automations } from "src/storage/schema";
+
+import type { CreateAutomationRecordInput } from "src/announcements/types/automations-source.types";
 
 @Injectable()
 export class AutomationsRepository {
   constructor(@Inject(DB) private readonly db: DatabasePg) {}
 
-  async createAutomation() {}
+  async createAutomation(input: CreateAutomationRecordInput) {
+    const [automation] = await this.db
+      .insert(automations)
+      .values({
+        name: input.name,
+        description: input.description,
+        status: input.status,
+      })
+      .returning();
+
+    return automation;
+  }
 }
