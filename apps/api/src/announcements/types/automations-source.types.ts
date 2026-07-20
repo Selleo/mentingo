f0aae4d909c24088df4f87c9c5aaedd1196649d5
pkrzysiekk <pkrzysiek24@gmail.com>
2026-07-20 11:11:59 +0200

    feat: Add AutomationStep type

--- apps/api/src/announcements/types/automations-source.types.ts
+++ apps/api/src/announcements/types/automations-source.types.ts
@@ -1,5 +1,12 @@
+import { Type } from "node_modules/@sinclair/typebox/build/cjs/type";
+import { createSelectSchema } from "node_modules/drizzle-typebox/index.cjs";
+
+import { automationSteps } from "src/storage/schema";
+import { omitTenantId } from "src/utils/omitTenantId";
+
 import type { AutomationStatus, AutomationType } from "./automations.types";
 import type { LocalizedText } from "node_modules/@repo/shared/dist/index.cjs";
+import type { Static } from "node_modules/@sinclair/typebox/build/cjs/type";
 import type { UUIDType } from "src/common";
 
 export type AutomationRecordInput = {
@@ -19,3 +26,10 @@ export type AutomationStepUpdateInput = {
   type: AutomationType;
   typeContext: JSON;
 };
+
+export const AutomationStepSchema = Type.Omit(omitTenantId(createSelectSchema(automationSteps)), [
+  "createdAt",
+  "updatedAt",
+]);
+
+export type AutomationStep = Static<typeof AutomationStepSchema>;
