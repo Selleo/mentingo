bbc02956228f19373e5f6c2493538a3aca4e37a3
Bartosz Cembala <bartosz.cembala@o2.pl>
2026-07-21 15:03:38 +0200

    fix: regenerate migrations

--- apps/api/src/storage/migrations/0171_add_course_overview_fields.sql
+++ apps/api/src/storage/migrations/0171_add_course_overview_fields.sql
@@ -1,3 +1,5 @@
+ALTER TABLE "ai_mentor_lessons" ALTER COLUMN "name" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "ai_mentor_lessons" ALTER COLUMN "name" SET DEFAULT 'AI Mentor';--> statement-breakpoint
 ALTER TABLE "courses" ADD COLUMN "thumbnail_position_y" integer DEFAULT 50 NOT NULL;--> statement-breakpoint
 ALTER TABLE "courses" ADD COLUMN "show_author_section" boolean DEFAULT true NOT NULL;--> statement-breakpoint
 ALTER TABLE "courses" ADD COLUMN "learning_outcomes" jsonb DEFAULT '{}'::jsonb NOT NULL;
