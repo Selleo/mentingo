301acd76acdf8a7962fb629cbc57b276a063feea
pkrzysiekk <pkrzysiek24@gmail.com>
2026-07-22 13:21:48 +0200

    feat: Add algorithm executing basic automation step tree

--- apps/api/src/automations/automation-runner/automation-runner.service.ts
+++ apps/api/src/automations/automation-runner/automation-runner.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 
 import { AutomationStepsService } from "../automations-steps/automations-steps.service";
 
+import type { AutomationStep } from "src/announcements/types/automations-source.types";
 import type { UUIDType } from "src/common";
 
 @Injectable()
@@ -9,7 +10,45 @@ export class AutomationRunnerService {
   constructor(private readonly automationStepsService: AutomationStepsService) {}
 
   async startAutomation(automationId: UUIDType) {
-    const automationSteps = this.automationStepsService.getAllAutomationSteps(automationId);
-    console.log(automationSteps);
+    const automationSteps = await this.automationStepsService.getAllAutomationSteps(automationId);
+    await this.executeAutomationSteps(automationSteps);
+  }
+  private async executeAutomationSteps(steps: AutomationStep[]) {
+    const root = steps.find((step) => step.parentId === null);
+
+    if (!root) {
+      throw new BadRequestException("automationSteps.toast.stepTreeBuildFailed");
+    }
+
+    await this.executeStep(root, steps);
+  }
+
+  private async executeStep(step: AutomationStep, steps: AutomationStep[]) {
+    await this.executeSingleStep(step);
+
+    const children = steps.filter((child) => child.parentId === step.id);
+
+    for (const child of children) {
+      await this.executeStep(child, steps);
+    }
+  }
+
+  private async executeSingleStep(step: AutomationStep) {
+    switch (step.type) {
+      case "trigger":
+        console.log("Trigger:", step.typeContext);
+        break;
+
+      case "condition":
+        console.log("Condition:", step.typeContext);
+        break;
+
+      case "action":
+        console.log("Action:", step.typeContext);
+        break;
+
+      default:
+        throw new BadRequestException(`Unknown step type ${step.type}`);
+    }
   }
 }
