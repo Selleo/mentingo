8700f3ca82c7588f48e228f05d22a62960ed5e5d
pkrzysiekk <pkrzysiek24@gmail.com>
2026-07-24 10:16:17 +0200

    fix: Add automation runner service test fix

--- apps/api/src/automations/automation-runner/automation-runner.service.spec.ts
+++ apps/api/src/automations/automation-runner/automation-runner.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test } from "@nestjs/testing";
 
 import { AutomationStepsService } from "../automations-steps/automations-steps.service";
+import { AutomationLogsRepository } from "../repositories/automation-logs/automation-logs";
 
 import { AutomationRunnerService } from "./automation-runner.service";
 
@@ -9,18 +10,33 @@ import type { TestingModule } from "@nestjs/testing";
 describe("AutomationRunnerService", () => {
   let service: AutomationRunnerService;
 
+  const automationStepsServiceMock = {};
+
+  const automationLogsRepositoryMock = {
+    create: jest.fn(),
+    GetByAutomationId: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         AutomationRunnerService,
         {
           provide: AutomationStepsService,
-          useValue: {},
+          useValue: automationStepsServiceMock,
+        },
+        {
+          provide: AutomationLogsRepository,
+          useValue: automationLogsRepositoryMock,
         },
       ],
     }).compile();
 
     service = module.get<AutomationRunnerService>(AutomationRunnerService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it("should be defined", () => {
