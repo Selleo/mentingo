fe36ccf7cafd7a32554e7e6eba5e527bb556b8b2
its-gabo <smolana.gabriel@gmail.com>
2026-08-26 09:55:21 +0200

    chore: improve rich text styling

--- apps/web/app/components/RichText/Viever.tsx
+++ apps/web/app/components/RichText/Viever.tsx
@@ -10,6 +10,7 @@ import {
   defaultClasses,
   contentVariantClasses,
 } from "./styles";
+import { normalizeMarkdownCode } from "./viewer.utils";
 import { RICH_TEXT_VIEWER_VARIANT, type RichTextViewerVariant } from "./viewerTypes";
 
 import type { SupportedLanguages } from "@repo/shared";
@@ -82,6 +83,7 @@ const Viewer = ({
 
   const editorClasses = cn(
     "h-full flex flex-col",
+    defaultClasses.codeBlock,
     defaultClasses.ul,
     defaultClasses.ol,
     defaultClasses.taskList,
@@ -99,10 +101,12 @@ const Viewer = ({
     );
   }, [handleVideoEnded, variant, videoCoverageTracking]);
 
+  const normalizedContent = useMemo(() => normalizeMarkdownCode(content), [content]);
+
   const editor = useEditor(
     {
       extensions,
-      content,
+      content: normalizedContent,
       editable: false,
       editorProps: {
         attributes: {
@@ -110,7 +114,7 @@ const Viewer = ({
         },
       },
     },
-    [content],
+    [normalizedContent],
   );
 
   if (!editor) return <></>;
